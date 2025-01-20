fccea3152724addc277b9e7aba1730ea69650c44
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-01-20 17:42:42 +0100

    Add files via upload

---
+++ Statements/src/Exer/ClassStatements.java
@@ -0,0 +1,15 @@
+package Exer;
+
+public class ClassStatements {
+
+	public static void main(String[] args) {
+		// Declara una variable de tipus int anomenada
+		// number i la inicialitza amb el valor 50
+		int number = 50;
+		// Multiplica per 3 el valor de number i l´assigna a number
+		number = number * 3;
+		// Imprimeix per pantalla el valor de number
+		System.out.println(number);
+	}
+
+}
