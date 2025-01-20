4826a48cd011e8149ce3f814364c04c93e48e6d2
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-01-20 12:05:45 +0100

    Add files via upload

---
+++ Comments/src/Modul_01/ClassName.java
@@ -0,0 +1,23 @@
+// Comentari d´una línia
+
+/* Comentari 
+ * de múltiples 
+ * línies
+ */
+
+/*
+ * Aquest codi es pot considerar una plantilla per començar
+ * a programar en Java.
+ */
+
+package Modul_01;
+
+// Aquest és el nom de la classe, trieu-ne un de representatiu.
+public class ClassName {   
+	// El main és un mètode/funció especial que fa de punt d´entrada del programa.
+	public static void main(String[] args) {
+		// de l´anglès to do, és a dir, per fer, pendent, etc.
+		// TODO: Aquí va el codi!!
+	}
+	// TODO: Aquí, hi podria anar més codi en forma de funcions (també anomenat mètodes).
+}
