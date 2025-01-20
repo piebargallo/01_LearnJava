95b80ff7da9489047a475d8bd510e1fa8aa930d0
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2025-01-20 17:41:58 +0100

    Add files via upload

---
+++ Out/src/Exer/ClassOut.java
@@ -0,0 +1,14 @@
+package Exer;
+
+public class ClassOut {
+
+	public static void main(String[] args) {
+		// Standard output stream
+		int number1 = 4;
+		int number2 = 8;
+		// És equivalent a system.out.print(4 + "\n");
+		System.out.println(number1);
+		System.out.print(number2);
+	}
+
+}
